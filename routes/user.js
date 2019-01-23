1965e39467455d2b3f9c924453c6ec2c141ab970
IERomanov <i.e.romanov1997@gmail.com>
2019-01-23 15:09:05 +0400

    a

--- routes/user.js
+++ routes/user.js
@@ -10,102 +10,86 @@ const validateLoginInput = require('../validation/login')
 const User = require('../model/User')
 
 router.post('/register', (req, res) => {
-    const { errors, isValid } = validateRegisterInput(req.body)
-    const { name, email, password } = req.body
+	const { errors, isValid } = validateRegisterInput(req.body)
+	const { name, email, password } = req.body
 
-    if(!isValid) return res.status(400).json(errors)
+	if (!isValid) return res.status(400).json(errors)
 
-    User.findOne({ email })
-        .then(user => {
-            if (user) return res.status(400).json(errors)
+	User.findOne({ email }).then(user => {
+		if (user) return res.status(400).json(errors)
 
-            const avatar = gravatar.url(email, {
-                s: '200',
-                r: 'pg',
-                d: 'mm'
-            })
-            const newUser = new User({
-                name,
-                email,
-                password,
-                avatar
-            })
+		const avatar = gravatar.url(email, {
+			s: '200',
+			r: 'pg',
+			d: 'mm',
+		})
+		const newUser = new User({
+			name,
+			email,
+			password,
+			avatar,
+		})
 
-            bcrypt.genSalt(10, ( err, salt ) => {
-                if (err) {
-                    console.error(err)
-                    return;
-                }
-                bcrypt.hash(
-                    newUser.password,
-                    salt,
-                    ( err, hash ) => {
-                        if (err) {
-                            console.error(err)
-                            return;
-                        }
-                        newUser.password = hash
-                        newUser.save().then(user => res.json(user))
-                })
-            })
-            res.status(200)
-        })
+		bcrypt.genSalt(10, (err, salt) => {
+			if (err) {
+				console.error(err)
+				return
+			}
+			bcrypt.hash(newUser.password, salt, (err, hash) => {
+				if (err) {
+					console.error(err)
+					return
+				}
+				newUser.password = hash
+				newUser.save().then(user => res.status(200).json(user))
+			})
+		})
+	})
 })
 
 router.post('/login', (req, res) => {
-    const { errors, isValid } = validateLoginInput(req.body)
-    const { email, password } = req.body
+	const { errors, isValid } = validateLoginInput(req.body)
+	const { email, password } = req.body
 
-    if(!isValid) return res.status(400).json(errors)
+	if (!isValid) return res.status(400).json(errors)
 
-    User.findOne({ email })
-        .then(user => {
-            if (!user) {
-                errors.email = 'User not found'
-                return res.status(404).json(errors)
-            }
+	User.findOne({ email }).then(user => {
+		if (!user) {
+			errors.email = 'User not found'
+			return res.status(404).json(errors)
+		}
 
-            bcrypt.compare(password, user.password)
-                .then(isMatch => {
-                    if (!isMatch) {
-                        errors.password = 'Incorrect password'
-                        return res.status(400).json(errors)
-                    }
+		bcrypt.compare(password, user.password).then(isMatch => {
+			if (!isMatch) {
+				errors.password = 'Incorrect password'
+				return res.status(400).json(errors)
+			}
 
-                    const payload = {
-                        id: user.id,
-                        name: user.name,
-                        avatar: user.avatar
-                    }
+			const payload = {
+				id: user.id,
+				name: user.name,
+				avatar: user.avatar,
+			}
 
-                    jwt.sign(
-                        payload,
-                        'secret',
-                        { expiresIn: 3600 },
-                        ( err, token ) => {
-                            if(err) {
-                                console.error(err)
-                                return;
-                            }
+			jwt.sign(payload, 'secret', { expiresIn: 3600 }, (err, token) => {
+				if (err) {
+					console.error(err)
+					return
+				}
 
-                            res.json({succes: true, token: `Bearer ${token}`})
-                        }
-                    )
-                })
-            res.status(200)
-        })
+				res.json({ succes: true, token: `Bearer ${token}` })
+			})
+		})
+		res.status(200)
+	})
 })
 
-router.get(
-    '/me',
-    passport.authenticate('jwt', { session: false }),
-    ( req, res ) => {
-        return res.json({
-            id: req.user.id,
-            name: req.user.name,
-            email: req.user.email
-        })
-    }
-)
+router.get('/me', passport.authenticate('jwt', { session: false }), (req, res) => {
+	return res.json({
+		id: req.user.id,
+		name: req.user.name,
+		email: req.user.email,
+	})
+})
 
-module.exports = router;
+module.exports = router
