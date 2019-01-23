92ffe95980d55bfaef01d24cd7d380027943c1e6
IERomanov <i.e.romanov1997@gmail.com>
2019-01-23 12:55:27 +0400

    build client

---
+++ public/precache-manifest.30fff3df64ea66929e491f9c1935e3a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d16357fc2e6304333179",
+    "url": "/static/js/main.d16357fc.chunk.js"
+  },
+  {
+    "revision": "a263ef1bb943458cfa87",
+    "url": "/static/js/1.a263ef1b.chunk.js"
+  },
+  {
+    "revision": "d16357fc2e6304333179",
+    "url": "/static/css/main.f3e56c13.chunk.css"
+  },
+  {
+    "revision": "a263ef1bb943458cfa87",
+    "url": "/static/css/1.35efbe8c.chunk.css"
+  },
+  {
+    "revision": "e06be009202cf027adc9e6fab9641eb6",
+    "url": "/index.html"
+  }
+];
