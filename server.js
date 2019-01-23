9eac2dce49cd6f8b1ce994d5283d75742a5315fc
IERomanov <i.e.romanov1997@gmail.com>
2019-01-23 14:26:20 +0400

    a

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ app.use('/api/todos', todos)
 
 app.get('*', function(req, res) {
 	res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+	console.log(path.resolve(__dirname, 'public', 'index.html'))
 })
 
 const PORT = process.env.PORT || 5000
