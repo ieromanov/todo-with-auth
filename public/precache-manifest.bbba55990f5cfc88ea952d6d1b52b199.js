2e287f6b4cc71392bada69db2f90edfffab1bd90
IERomanov <i.e.romanov1997@gmail.com>
2019-01-24 12:03:22 +0400

    fix after build

---
+++ public/precache-manifest.bbba55990f5cfc88ea952d6d1b52b199.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "12d001b64e6146a1c95e",
+    "url": "/static/js/main.12d001b6.chunk.js"
+  },
+  {
+    "revision": "33b040d226aedfb0a31d",
+    "url": "/static/js/1.33b040d2.chunk.js"
+  },
+  {
+    "revision": "12d001b64e6146a1c95e",
+    "url": "/static/css/main.fd78db58.chunk.css"
+  },
+  {
+    "revision": "33b040d226aedfb0a31d",
+    "url": "/static/css/1.90f41fd3.chunk.css"
+  },
+  {
+    "revision": "bbc896ca02c6decf3155039a2185099f",
+    "url": "/index.html"
+  }
+];
